aae5bc229fbe945e78f4e8958f571e257a4a8ae1
Ola Ajibode <ola.ajibode@gmail.com>
2021-11-08 17:38:31 +0000

    Feat: Read MDX files into pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,26 +1,34 @@
 // Step 1: Import React
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/Layout';
 
 // Step 2: Define your component
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='Blog Posts'>
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
